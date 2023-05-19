1b45aaa56be82ca2af5fcb80215c1121716fb0ab
WildBarreto <wildsbsrreto@gmail.com>
2023-05-19 10:45:06 -0300

    Rotas mobile

---
+++ app/memories.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Memories() {
+  return (
+    <View className="items-center flex-1 justify-center">
+      <Text>Memories</Text>
+    </View>
+  );
+}
