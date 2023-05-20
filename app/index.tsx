5cc6c8e1193c550a3d41505f31e8fbf91c701a1a
WildBarreto <wildsbsrreto@gmail.com>
2023-05-20 11:49:55 -0300

    navegação

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,11 @@
 import * as SecureStore from "expo-secure-store";
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
-
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from "@expo-google-fonts/roboto";
+import { Text, TouchableOpacity, View } from "react-native";
 
 import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { useRouter } from "expo-router";
 import { styled } from "nativewind";
 import { useEffect } from "react";
 
-import blurBg from "../src/assets/bg-blur.png";
 import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 import Stripes from "../src/assets/stripes.svg";
 import { api } from "../src/lib/api";
@@ -30,12 +21,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter();
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  });
 
   const [request, response, signInWithGithub] = useAuthRequest(
     {
@@ -55,7 +40,7 @@ export default function App() {
     const { token } = response.data;
     await SecureStore.setItemAsync("token", token);
 
-    router.push('/memories')
+    router.push("/memories");
   }
 
   useEffect(() => {
@@ -70,20 +55,8 @@ export default function App() {
     }
   }, [response]);
 
-  if (!hasLoadedFonts) {
-    return null;
-  }
-  
-
   return (
-    <ImageBackground
-      source={blurBg}
-      imageStyle={{ position: "absolute", left: "-100%" }}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-    >
-      <StyledStripes className="absolute left-2" />
-      <StatusBar style="light" translucent />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
@@ -108,6 +81,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito no NLW
       </Text>
-    </ImageBackground>
+    </View>
   );
 }
